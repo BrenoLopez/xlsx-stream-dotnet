745f0601711097995bfa2508306ed89b198693a6
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-04-22 08:29:11 -0300

    feat: create xlsx file from IAsyncEnumerable

--- src/Report.Console/Program.cs
+++ src/Report.Console/Program.cs
@@ -1,26 +1,28 @@
-﻿//using SpreadCheetah;
+﻿using SpreadCheetah;
 
-//using var fileStream = File.Create("./report.xlsx");
+using var fileStream = File.Create("./report.xlsx");
 
-//using var spreadsheet = await Spreadsheet.CreateNewAsync(fileStream);
+using var spreadsheet = await Spreadsheet.CreateNewAsync(fileStream);
 
-//await spreadsheet.StartWorksheetAsync("Sheet 1");
+await spreadsheet.StartWorksheetAsync("Sheet 1");
 
-//Cell[] row = [new("Lorem ipsum dolor siamet"), new(10)];
+await foreach (var record in GetRecordsAsync())
+{
+    Cell[] row = [new(record.Description), new(record.Stars)];
+    await spreadsheet.AddRowAsync(row);
+}
 
-//await spreadsheet.AddRowAsync(row);
+await spreadsheet.FinishAsync();
 
-//await spreadsheet.FinishAsync();
+//using CsvHelper;
 
-using CsvHelper;
+//using System.Globalization;
 
-using System.Globalization;
+//using var writer = new StreamWriter("./report.csv");
 
-using var writer = new StreamWriter("./report.csv");
+//using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
-using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-
-await foreach (var record in GetRecordsAsync()) csv.WriteRecord(record);
+//await foreach (var record in GetRecordsAsync()) csv.WriteRecord(record);
 
 static async IAsyncEnumerable<SampleRecord> GetRecordsAsync()
 {
