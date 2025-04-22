7a481610859396a190368806058df0992f7e5b9e
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-04-22 08:00:13 -0300

    chore: init repo

---
+++ src/Report.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Hello, World!");
+using SpreadCheetah;
+
+using var fileStream = File.Create("./report.xlsx");
+
+using var spreadsheet = await Spreadsheet.CreateNewAsync(fileStream);
+
+await spreadsheet.StartWorksheetAsync("Sheet 1");
+
+Cell[] row = [new("Lorem ipsum dolor siamet"), new(10)];
+
+await spreadsheet.AddRowAsync(row);
+
+await spreadsheet.FinishAsync();
