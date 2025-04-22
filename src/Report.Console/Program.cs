e3cb5a28acd552705cff2c4ba03f475872ef2a18
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-04-22 14:47:23 -0300

    feat: generate xlsx with fs using spreadcheetah

--- src/Report.Console/Program.cs
+++ src/Report.Console/Program.cs
@@ -1,9 +1,43 @@
-﻿var records = ReportInMemoryRepository.GetRecordsAsync(1_000_000);
+﻿using SpreadCheetah;
 
-var generator = new ExcelReportGenerator(new AmazonS3Client("test", "test", new AmazonS3Config
+class Program
 {
-    ServiceURL = "http://localhost:4566",
-    ForcePathStyle = true,
-}));
+    static async Task Main(string[] args)
+    {
+        string filePath = "./teste_spreadcheetah.xlsx";
+        int numberOfSheets = 10;
 
-await generator.GenerateAsync(records);
+        await CreateLargeSpreadsheetWithSpreadCheetah(filePath, numberOfSheets);
+    }
+
+    static async Task CreateLargeSpreadsheetWithSpreadCheetah(string filePath, int numberOfSheets)
+    {
+        using (var stream = File.Create(filePath))
+        {
+            var spreadsheet = await Spreadsheet.CreateNewAsync(stream);
+
+            for (int sheetIndex = 0; sheetIndex < numberOfSheets; sheetIndex++)
+            {
+                await spreadsheet.StartWorksheetAsync($"Sheet{sheetIndex + 1}");
+
+                for (int rowIndex = 0; rowIndex < 1_000_000; rowIndex++)
+                {
+                    var row = new Cell[40];
+                    for (int col = 0; col < 40; col++)
+                    {
+                        row[col] = new("test"); 
+                    }
+
+                    await spreadsheet.AddRowAsync(row);
+
+                    if ((rowIndex + 1) % 10_000 == 0)
+                    {
+                        Console.WriteLine($"Sheet {sheetIndex + 1} - linhas escritas: {rowIndex + 1}");
+                    }
+                }
+            }
+
+            await spreadsheet.FinishAsync();
+        }
+    }
+}
